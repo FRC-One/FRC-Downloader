4e7bcb0433d608207ad2f4d9651991ce22c387f9
Alex Beaver <alex@abvr.xyz>
2020-01-03 05:24:19 -0800

    Change to Powershell

---
+++ Downloader.ps1
@@ -0,0 +1,34 @@
+﻿Import-Module BitsTransfer
+
+$P = Import-Csv -Path .\FRCSoftware2020.csv
+$P | Format-Table
+
+
+ForEach ($entry in $P){
+
+	
+	$friendlyName = $entry.("FriendlyName")
+	$fileName = $entry.("FileName")
+	$zipped = $entry.("isZipped")
+
+	$url = $entry.("URL")
+	$output = "$PSScriptRoot\$fileName"
+	Write-Output "Downloading $friendlyName"
+
+	Start-BitsTransfer -Source $url -Destination $output
+
+	If($zipped -eq "TRUE"){
+		$unzipOutput = "$PSScriptRoot\$friendlyName"
+		Write-Output "Uncompressing $friendlyName to $unzipOutput"
+
+		Expand-Archive -LiteralPath $output -DestinationPath $unzipOutput
+	}
+	Write-Output "Finished $friendlyName"
+
+}
+
+
+
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+
--- README.md
+++
@@ -1,3 +0,0 @@
-# FRC-Downloader
-
-An electron based package downloader for FRC based on [CSA-USB-Tool](https://github.com/JamieSinn/CSA-USB-Tool)
